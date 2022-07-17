bdfc5862be470b0502c8e4847ffefed3da523d6f
Marcel <mk@mkarrasch.de>
2022-07-17 17:16:53 +0200

    Update Euler 7

--- Euler007.py
+++ Euler007.py
@@ -12,30 +12,23 @@ from time import *
 
 # Checks if a number is a prime
 def is_prime(n):
-    count = 0
-    for i in range(2, n):
+    for i in range(2, round(math.sqrt(n) + 1)):
         if n % i == 0:  
             return False
-            break
-        else:
-            count += 1
-    if count == n - 2:
-        return True
+    return True
 
-
-# Finds the value for the given nth term
-def term(n):
-    x = 0
-    count = 0
-    while count != n:
-        x += 1
+# Finds the value for the given n-th term
+def find(max):
+    x = 2
+    count = 1
+    while count <= max:
         if is_prime(x):
+            print(x)
             count += 1
-    print(x)
-
+        x += 1
 
 t1 = time()
-term(10001)
+find(10001)
 t2 = time()
 
 print('Running for: ', round(t2 - t1, 4), 'sec')
